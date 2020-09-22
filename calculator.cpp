ac5e0fa71627decd8a91a9ad5d7682dad7cd7dc8
Nuclearl <56044025+Nuclearl@users.noreply.github.com>
2020-09-22 21:30:57 +0300

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,20 @@
+#include "calculator.h"
+
+int Calculator::Add (int a, int b)
+{
+    return a + b;
+}
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
+
+// text
+// text
+// text
+// text
+// text
+// text
+// text
+// text
